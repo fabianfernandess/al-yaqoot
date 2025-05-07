a177282020d82aec7cac0236937305699a5c7873
fabianfernandess <fabianfernandes62@gmail.com>
2025-05-08 00:39:42 +0400

    made the navbar radient and changed the font of home etc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,8 @@ import "../styles/home.css";
 const Home = () => (
   <section className="home-hero">
     <div className="hero-left">
-      <h1>The Future of Signage</h1>
-      <p>From Design to installation, <br /> We bring signs to life</p>
+      <h1>Dubai’s Leading Signage Company<br/> Custom Signs That Make an Impact</h1>
+      <p>As a trusted signboard company in Dubai, we specialize in end-to-end signage solutions – from design and fabrication to installation. Whether you need indoor branding, outdoor signage, or vehicle graphics, our team delivers high-quality, customized signs that enhance your visibility and elevate your brand.</p>
       <button className="cta-btn">Get in touch</button>
     </div>
     <div className="hero-right">
